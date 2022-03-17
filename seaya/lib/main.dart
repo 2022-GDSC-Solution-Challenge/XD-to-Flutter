18a0c5f742fec9ebb8f2ed5558ffbf652ec3cbfc
YounJ00 <hamyounjin006@gmail.com>
2022-03-17 15:01:53 +0900

    newsDialog

--- seaya/lib/main.dart
+++ seaya/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:seaya/screens/Routes/Navigation.dart';
+import 'package:seaya/screens/Routes/newsDialog.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,7 +14,7 @@ class MyApp extends StatelessWidget {
     return const MaterialApp(
       title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
-      home: Navigation(),
+      home: newsDialog(),
     );
   }
 }
